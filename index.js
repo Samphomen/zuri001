d87a397ba614b015e882dbdc2bfe768e177804d8
Samphomen <ishaqcole@gmail.com>
2023-09-10 10:16:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Endpoint route
+app.get('/api', (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+  const utcTime = new Date().toISOString();
+
+  const githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+  const githubRepoUrl = 'https://github.com/Samphomen/zuri001.git';
+
+
+  const response = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: 200,
+  };
+
+  res.status(200).json(response);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
