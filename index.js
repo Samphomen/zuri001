ec163d7bf21d03f057fb33c69435d32d76258bab
Samphomen <ishaqcole@gmail.com>
2023-09-10 11:10:28 +0100

    fifth commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const moment = require('moment')
 const port = process.env.PORT || 5000;
 
 // Endpoint route
 app.get('/api', (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
+  const now = moment();
 
   const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
   const utcTime = new Date().toISOString();
@@ -13,21 +15,10 @@ app.get('/api', (req, res) => {
   const githubFileUrl = 'https://github.com/Samphomen/zuri001/blob/master/index.js';
   const githubRepoUrl = 'https://github.com/Samphomen/zuri001.git';
 
-
-  const response = {
-    slack_name: slackName,
-    current_day: currentDay,
-    utc_time: utcTime,
-    track: track,
-    github_file_url: githubFileUrl,
-    github_repo_url: githubRepoUrl,
-    status_code: 200,
-  };
-
   res.status(200).json({
     slack_name: slackName,
     current_day: currentDay,
-    utc_time: utcTime,
+    utc_time: now.utc().format(),
     track: track,
     github_file_url: githubFileUrl,
     github_repo_url: githubRepoUrl,
